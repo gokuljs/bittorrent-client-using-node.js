d4b3d7b76fbf4ab1c1c8f55c071f1cb56f87201b
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-09-16 20:26:12 +0530

    buffers

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-'use strict';
-const fs = require('fs');
-const torrent = fs.readFileSync('puppy.torrent');
-console.log(torrent.toString('utf8'));
+'use strict'; // "use strict"; Defines that JavaScript code should be executed in "strict mode".
+//With strict mode, you can not, for example, use undeclared variables.
+const fs = require('fs'); //The fs module enables interacting with the file system in a way modeled on standard POSIX functions.
+const torrent = fs.readFileSync('puppy.torrent'); //readFileSync is the easiest way to read the contents of a file
+console.log(torrent);
