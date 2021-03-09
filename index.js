292f2fd88ea6b93314a5f12c086143be58d2b2bc
Gokul js <51263906+gokuljs@users.noreply.github.com>
2021-03-09 23:02:57 +0530

    .

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-'use strict';
-const fs = require('fs');
-const torrent = fs.readFileSync('test.torrent');
-console.log(torrent.toString('utf8'));
+'use strict'; //With strict mode, you can not, for example, use undeclared variables.
+// normally torrent uses bencode format so know we are importing the becode 
+const bencode = require("bencode");
+const fs = require('fs'); // package used for interacting with file
+const torrent = bencode.decode(fs.readFileSync('test.torrent')); // read file sync is used to read contents of the file 
+// torrent = bencode.decode(torrent);
+console.log(torrent)
+console.log(torrent.announce.toString('utf8')); // read file sync gives the output in buffer converting it into an standard format utf8
+// normally netwrok messages are sent and read through buffers
